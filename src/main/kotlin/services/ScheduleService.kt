a897166f2bf6120a744e0a662d123f2f2a7b6017
Lukas Hofwimmer <lukas.hofwimmer@gmail.com>
2020-07-26 23:11:24 +0200

    ADD getSchedules function ADD getAllSchedules function

--- src/main/kotlin/services/ScheduleService.kt
+++ src/main/kotlin/services/ScheduleService.kt
@@ -2,10 +2,13 @@ package main.kotlin.services
 
 import main.kotlin.model.db.Employee
 import main.kotlin.model.db.Schedule
+import main.kotlin.model.db.Schedules
+import main.kotlin.model.db.User
 import main.kotlin.model.db.toDataClass
 import main.kotlin.model.dto.ScheduleResponse
+import main.kotlin.net.Registry
+import org.jetbrains.exposed.sql.select
 import org.jetbrains.exposed.sql.transactions.transaction
-import org.jetbrains.exposed.sql.transactions.transactionScope
 import org.joda.time.DateTime
 
 class ScheduleService {
@@ -14,7 +17,7 @@ class ScheduleService {
         scheduleStart: DateTime,
         scheduleEnd: DateTime
     ): ScheduleResponse {
-        val schedule = transaction{
+        val schedule = transaction {
             Schedule.new {
                 this.employee = employee
                 this.scheduleStart = scheduleStart
@@ -25,5 +28,27 @@ class ScheduleService {
         return schedule.toDataClass()
     }
 
+    fun getSchedules(user: User, employeeUUID: String?, limit: Int, offset: Long): List<ScheduleResponse> {
+        if (employeeUUID == null) return getAllSchedules(limit, offset)
+        val employee = Registry.employeeService.findByUUID(user, employeeUUID) ?: return getAllSchedules(limit, offset)
 
+        return transaction {
+            Schedules.select {
+                Schedules.employee eq employee.id
+            }.limit(limit, offset = offset)
+        }.map { row ->
+            val rowEmployee = Employee.findById(row[Schedules.employee]) ?: return listOf()
+
+            ScheduleResponse(
+                row[Schedules.uuid].toString(),
+                row[Schedules.scheduleStart].millis,
+                row[Schedules.scheduleEnd].millis,
+                rowEmployee.toDataClass()
+            )
+        }
+    }
+
+    fun getAllSchedules(limit: Int, offset: Long): List<ScheduleResponse> {
+        return Schedule.all().limit(limit, offset).map(Schedule::toDataClass)
+    }
 }
