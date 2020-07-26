a2d0f0cb4a24a47b3227f773bf8a7a5f127c1237
Lukas Hofwimmer <lukas.hofwimmer@gmail.com>
2020-07-26 22:56:55 +0200

    ADD geEmployees function ADD constraint for current user

--- src/main/kotlin/services/EmployeeService.kt
+++ src/main/kotlin/services/EmployeeService.kt
@@ -8,6 +8,8 @@ import main.kotlin.model.db.toDataClass
 import main.kotlin.model.dto.EmployeeRequest
 import main.kotlin.model.dto.EmployeeResponse
 import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
+import org.jetbrains.exposed.sql.and
+import org.jetbrains.exposed.sql.select
 import org.jetbrains.exposed.sql.transactions.transaction
 import org.joda.time.DateTime
 import java.math.BigDecimal
@@ -31,7 +33,27 @@ class EmployeeService {
         }
     }.toDataClass()
 
-    fun findByUUID(uuid: String) = transaction {
-        Employee.find { Employees.uuid eq UUID.fromString(uuid) }.firstOrNull()
+    fun findByUUID(user: User, uuid: String) = transaction {
+        Employee.find { (Employees.uuid eq UUID.fromString(uuid)) and
+            (Employees.employer eq user.id) }.firstOrNull()
+    }
+
+    fun getEmployees(user: User, limit: Int, offset: Long) : List<EmployeeResponse> {
+        return transaction {
+            Employees.select {
+                Employees.employer eq user.id
+            }.limit(limit,offset)
+        }.map { row ->
+            val employer = User.findById(row[Employees.employer]) ?: return listOf()
+
+            EmployeeResponse(
+                row[Employees.uuid].toString(),
+                row[Employees.weeklyHours],
+                row[Employees.entryDate]?.millis,
+                row[Employees.firstname],
+                row[Employees.lastname],
+                employer.uuid.toString()
+            )
+        }
     }
 }
